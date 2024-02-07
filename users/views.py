2c1bf0321e9232dca241192ec9eac3e2046ebe49
njsaugat <njsaugat@gmail.com>
2024-02-07 15:20:14 +0545

    views

--- users/views.py
+++ users/views.py
@@ -1,10 +1,8 @@
 from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
 from allauth.socialaccount.providers.oauth2.client import OAuth2Client
-from decouple import config
 from dj_rest_auth.registration.views import RegisterView, SocialLoginView
 from dj_rest_auth.views import LoginView
 from django.contrib.auth import get_user_model
-from django.core.mail import EmailMessage
 from django.utils.translation import gettext as _
 from rest_framework import permissions, status
 from rest_framework.generics import (GenericAPIView, RetrieveAPIView,
@@ -12,14 +10,13 @@ from rest_framework.generics import (GenericAPIView, RetrieveAPIView,
 from rest_framework.response import Response
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
-from users.models import Address, PhoneNumber, Profile
+from users.models import Address, OTP, Profile,User
 from users.permissions import IsUserAddressOwner, IsUserProfileOwner
 from users.serializers import (AddressReadOnlySerializer,
                                PhoneNumberSerializer, ProfileSerializer,
                                UserLoginSerializer, UserRegistrationSerializer,
-                               UserSerializer, VerifyPhoneNumberSerializer)
+                               UserSerializer, VerifyPhoneNumberSerializer,EmailSerializer,VerifyEmailSerializer)
 
-User=get_user_model()
 
 
 class UserRegistrationAPIView(RegisterView):
@@ -28,16 +25,7 @@ class UserRegistrationAPIView(RegisterView):
     """
 
     serializer_class=UserRegistrationSerializer
-
-    @staticmethod
-    def send_email(data):
-        email=EmailMessage(
-            subject=data['subject'],
-            body=data['body'],
-            from_email=config('EMAIL_FROM'),
-            to=[data['to_email']]
-        )
-        email.send()
+    
     
     def send_message(self,request,message,*args,**kwargs):
         res=SendOrResendSMSAPIView.as_view()(request._request,*args,**kwargs)
@@ -46,7 +34,6 @@ class UserRegistrationAPIView(RegisterView):
             return {"detail":message}
         
 
-    # inherit from genericAPIview -->so overwrite create method
     def create(self,request,*args,**kwargs):
         serializer=self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
@@ -59,14 +46,14 @@ class UserRegistrationAPIView(RegisterView):
         email=request.data.get("email",None)
         phone_number=request.data.get("phone_number",None)
 
-        if email and phone_number:
-            response_data=self.send_message(request,message=_("Verification e-mail and SMS sent."),*args,**kwargs)
-            
-        elif email and not phone_number:
-            response_data={"detail":_("verification email sent.")}
-
-        else:
+        if phone_number:
             response_data=self.send_message(request,message=_("Verification SMS sent."),*args,**kwargs)
+        else:
+            res=SendOrResendEmailOTPView.as_view()(request._request,*args,**kwargs)
+
+            if res.status_code==200:
+                response_data={"detail":_("Verification e-mail sent.")}
+                
         
         return Response(response_data,status=status.HTTP_201_CREATED,headers=headers)
 
@@ -84,7 +71,6 @@ class SendOrResendSMSAPIView(GenericAPIView):
     """
     Check if submitted phone number is valid phone number and send OTP
     """
-    # this is where we call the twilio
     
     serializer_class=PhoneNumberSerializer
     
@@ -96,16 +82,61 @@ class SendOrResendSMSAPIView(GenericAPIView):
                     
         phone_number=str(serializer.validated_data["phone_number"])
 
-        user=User.objects.filter(phone__phone_number=phone_number).first()
+        user=User.objects.filter(phone_number=phone_number).first()
         
-        sms_verification=PhoneNumber.objects.filter(
+        sms_verification=OTP.objects.filter(
             user=user,is_verified=False
         ).first()
 
         sms_verification.send_confirmation()
     
         return Response(status=status.HTTP_200_OK)
+ 
+ 
+ 
+class SendOrResendEmailOTPView(GenericAPIView):
+    
+    """
+    Check if submitted phone number is valid phone number and send OTP
+    """
+    
+    serializer_class=EmailSerializer
+    
+    def post(self,request,*args,**kwargs):
+        serializer=self.get_serializer(data=request.data)
+
+        if not serializer.is_valid():
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+                    
+        email=str(serializer.validated_data["email"])
+
+        is_otp_for_password=str(serializer.validated_data["is_otp_for_password"])
+
+        user=User.objects.filter(email=email).first()
+
+        otp_code=None
+
+        if is_otp_for_password:
+            otp_code=OTP.objects.filter(user=user).first()
+            if otp_code is None:
+                otp_code=OTP(user=user)
+            otp_code.send_email_OTP(reciever_email=email)
+            
+            return Response(status=status.HTTP_200_OK)        
+            
         
+        
+        otp_code=OTP.objects.filter(
+            user=user,is_verified=False
+        ).first()
+        
+        if otp_code is None:
+            otp_code=OTP(user=user)
+
+        otp_code.send_email_OTP(reciever_email=email)
+
+        
+        return Response(status=status.HTTP_200_OK)        
 
 class VerifyPhoneNumberAPIView(GenericAPIView):
     """
@@ -121,6 +152,22 @@ class VerifyPhoneNumberAPIView(GenericAPIView):
             return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
 
         message={"detail":_("Phone number successfully verified.")}
+        return Response(message,status=status.HTTP_200_OK)
+
+class VerifyEmailAPIView(GenericAPIView):
+    """
+    Check if submitted phone number and OTP matches and verify the user
+    """
+
+    serializer_class=VerifyEmailSerializer
+    
+    def post(self,request,*args,**kwargs):
+        serializer=self.get_serializer(data=request.data)
+
+        if not serializer.is_valid():
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+        message={"detail":_("Email successfully verified.")}
         return Response(message,status=status.HTTP_200_OK)
 
 
