67767772078309a88d9e1d4c800e53b951459fe2
njsaugat <njsaugat@gmail.com>
2024-02-07 15:19:38 +0545

    urls

--- users/urls.py
+++ users/urls.py
@@ -8,7 +8,8 @@ from .views import(
     UserAPIView,
     UserLoginAPIView,
     UserRegistrationAPIView,
-    VerifyPhoneNumberAPIView
+    VerifyPhoneNumberAPIView,
+    VerifyEmailAPIView
 )
 
 app_name="users"
@@ -18,9 +19,10 @@ router.register(r"",AddressViewSet)
 
 urlpatterns = [
     path("register/",UserRegistrationAPIView.as_view(),name="user_register"),
+    path("verify-otp/",VerifyEmailAPIView.as_view(),name="verify_otp"),
+    path("verify-phone/",VerifyPhoneNumberAPIView.as_view(),name="verify_phone_number"),
     path("login/",UserLoginAPIView.as_view(),name="user_login"),
     path("send-sms/",SendOrResendSMSAPIView.as_view(), name="send_resend_sms"),
-    path("verify-phone/",VerifyPhoneNumberAPIView.as_view(),name="verify_phone_number"),
     path("",UserAPIView.as_view(),name="user_detail"),
     path("profile/",ProfileAPIView.as_view(),name="profile_detail"),
     path("profile/address/",include(router.urls))
